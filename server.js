a4e09e782cf388bee7d242a4c16dd4c6a585b3b1
José Daniel Pereira <daniel_horacio1@hotmail.com>
2019-12-09 16:04:29 +0000

    pocfile

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const port = process.env.PORT | 3000;
-const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT | 8080;
+//const host = process.env.HOST || '127.0.0.1';
 const express = require('express');
 
 const app = express();
