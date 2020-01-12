0abe32655f0676d6c2e282c7d9d74b9a823eabed
José Daniel Pereira <daniel_horacio1@hotmail.com>
2020-01-12 04:16:05 +0000

    ex

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -13,10 +13,10 @@ const jsonMessagesPath = __dirname + "/../assets/jsonMessages/";
 const jsonMessages = require(jsonMessagesPath + "login");
 
 router.get('/', function(req, res) {  //rota express....
-   //res.send(path.resolve(__dirname + '/../../Front-end-gestor-espaco/'))
+   res.send(path.resolve(__dirname + '/../../Front-end-gestor-espaco/'))
 
-   res.send("PW");
-   res.end();
+   //res.send("PW");
+   //res.end();
 });
 
 
