8732b390559eebba4b32632c855064db0e9fe64f
José Daniel Pereira <daniel_horacio1@hotmail.com>
2020-01-12 16:21:53 +0000

    r

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -13,7 +13,7 @@ const jsonMessagesPath = __dirname + "/../assets/jsonMessages/";
 const jsonMessages = require(jsonMessagesPath + "login");
 
 router.get('/', function(req, res) {  //rota express....
-   res.send(path.resolve(__dirname + 'https://spacemanager.github.io/'))
+   //res.send(path.resolve(__dirname + '/../../Front-end-gestor-espaco/'))
 
    res.send("PW");
    //res.end();
